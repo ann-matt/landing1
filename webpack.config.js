fb2ea2ed2cde512f6d3b8453a6420bd575d43c61
Ann Matt <anmatyushkova@gmail.com>
2025-05-13 15:41:25 +0300

    copy plagin download and set

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -11,13 +12,27 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: './src/index.html'
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/img'),
+                    to: path.resolve(__dirname, 'img')
+                }
+            ]
         })
     ],
+
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test:/\.png$/,
+                use: ['file-loader']
             }
         ]
     }
